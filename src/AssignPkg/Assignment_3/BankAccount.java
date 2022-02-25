99acb894e634957f85e2d60e2eaf7abe0bf958da
Faltu-Programmer <bsuman1921@gmail.com>
2022-02-26 03:20:53 +0530

    BankAccount class is now complete, but i would like to add a new feature later, which is recording the history and activety upto 100 , this commit is for the reminder to make the feature later

--- src/AssignPkg/Assignment_3/BankAccount.java
+++ src/AssignPkg/Assignment_3/BankAccount.java
@@ -1,10 +1,28 @@
+/*
+    7. Design a class to represent a Bank Account. Include the following things:
+
+    Fields
+        Name of the depositor
+        Account number
+        Type of account
+        Balance amount in the account.
+
+    Constructor
+        To assign initial values
+
+    Methods
+        To deposit an amount
+        To withdraw an amount after checking balance
+        To display the name and balance
+
+*/
 package AssignPkg.Assignment_3;
 
 public class BankAccount
 {
     private final String  m_accountHolderName;
     private static int m_accountNumber = -1; /// for testing purpose account number take between 100 to 150 means three digits only ..
-    private final String m_typeOfAccount;  /// 1 type is savings and 2 is current 
+    private String m_typeOfAccount;  /// 1 type is savings and 2 is current 
     private int m_balance = -1; 
     
     public BankAccount () 
@@ -17,7 +35,7 @@ public class BankAccount
     public BankAccount (String t_depositorName,  int t_initialBal ) 
     {
        m_accountHolderName = t_depositorName;
-        m_typeOfAccount = "savings";
+        m_typeOfAccount = "Savings";
         m_balance = t_initialBal;
         m_accountNumber = (m_accountNumber <= 0)? 100 : ++m_accountNumber;
     }
@@ -29,7 +47,7 @@ public class BankAccount
         m_accountNumber = (m_accountNumber <= 0)? 100 : ++m_accountNumber;
     }
     /*
-    /// this feature will not be implemented beacause one accountHolder can have
+    /// this feature can not be implemented beacause one accountHolder can have
     /// one account only
     public BankAccount (BankAccount t_refObj) 
     {
@@ -54,26 +72,27 @@ public class BankAccount
         return m_typeOfAccount;
     }
     
-    /*
+    
     /// this feature will be added after the completion of the class
     /// this feature will use when account holder want to change their account
     /// type . it will cost him 100 rupees only...
     private void setTypeOfAccount ()
     {
-        m_typeOfAccount = (m_typeOfAccount.equals("current")) ? "savings" : "current";
+        m_typeOfAccount = (m_typeOfAccount.equals("Current")) ? "Savings" : "Current";
     }
-    */
+    
     public int getBalance ()
     {
         return m_balance;
     }
-    public void setBalance (int t_balance)
+    private void setBalance (int t_balance)
     {
         m_balance = (m_balance < 0) ? 0 : t_balance;
     }
     
     public void showAccountDetails ()
     {
+        System.out.println();
         System.out.println("Name of the Account Holder is Mr./Mrs. " + getAccountHolderName ());
         System.out.println("Type of Account is *" + getTypeOfAccount () + " account");
         System.out.println("The Account Number is " + getAccountNumber ());
@@ -89,6 +108,7 @@ public class BankAccount
             System.out.println("Successfully Deposite \u20B9" + t_depositeAmmount);
         }
     }
+    
     public void withdraw (int t_withdrawlAmmount)
     {
         int tmpBalance = getBalance ();
@@ -100,12 +120,26 @@ public class BankAccount
         }
         else
         {
-            System.out.println("Insufficient Balance");
+           insufficientBalance ();
         }
     }
-    public void balance ()
+    
+    public final void balance ()
     {
         System.out.println("Your Balance is \u20B9" + getBalance () );
+    }
+    
+    public void changeAccountType ()
+    {
+        if(getBalance() > 100)
+            setTypeOfAccount ();
+        insufficientBalance ();
+        
+        ///(getBalance() > 100) ? setTypeOfAccount (): insufficientBalance ();
+    }
+    private void insufficientBalance ()
+    {
+         System.out.println("Insufficient Balance");
     }
 }
 
