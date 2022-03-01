cae0b00c7bd327c30c3b586480bd5f2fdce352b9
Faltu-Programmer <bsuman1921@gmail.com>
2022-03-01 22:56:29 +0530

    random exprtiment

---
+++ src/AssignPkg/SevaralProblems/EvenClaculator.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package AssignPkg.SevaralProblems;
+
+/**
+ *
+ * @author suman
+ */
+public class EvenClaculator
+{
+    public int count (int t_param)
+    {
+        int cnt = 0;
+        while (t_param >= 1)
+        {
+            ++cnt;
+        }
+        return cnt;
+    }
+}
